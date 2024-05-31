f16bb49b14cce7ab37c7233dae9dc716a180305e
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-05-31 10:22:10 -0300

    Estilos Dentro do Template (Inline Styles)

--- 02_Componentes_Estilizacoes/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
+++ 02_Componentes_Estilizacoes/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button-cancel',
-  template: `
-    <div class="card-button-cancel">Cancelar</div>
-  `,
-  styleUrls: [
-    './card-button-cancel.component.scss',
-    './card-button-cancel-2.component.scss'
-  ]
+  templateUrl: './card-button-cancel.component.html',
+  styleUrls: ['./card-button-cancel.component.scss']
 })
 export class CardButtonCancelComponent {
 
