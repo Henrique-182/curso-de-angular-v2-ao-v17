4cc3397adf8f2eae25bb34b3bddedb5fe44b7a7d
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-13 09:03:32 -0300

    NgIf

--- 03_Binding_Diretivas_Templates_Decorators/ngif/src/app/app.component.ts
+++ 03_Binding_Diretivas_Templates_Decorators/ngif/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  minhaProp = true
+  pessoa: any = {
+    status: true,
+    name: ''
+  }
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp
+  }
+
+  isTruthy() {
+    // return {}
+    // return []
+    // return true
+    // return 1
+    // return "teste"
+    // return '0'
+    // return "false"
+    return this.pessoa.status
+  }
+
+  isFalsy() {
+    // return false
+    // return ""
+    // return 0
+    // return null
+    // return undefined
+    // return -0
+    // return NaN
+    // return this.pessoa.name
+    return this.pessoa.endereco?.rua
+  }
 }
