e0835a3ff46554499448a94280c4978bf1a9589d
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-13 09:44:18 -0300

    NgFor

--- 03_Binding_Diretivas_Templates_Decorators/ngfor/src/app/app.component.ts
+++ 03_Binding_Diretivas_Templates_Decorators/ngfor/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  personSelectedIndex: number | undefined
+
+  listPeople = [
+    {
+      name: 'Pessoa 1',
+      age: 10
+    },
+    {
+      name: 'Pessoa 2',
+      age: 20
+    },
+    {
+      name: 'Pessoa 3',
+      age: 30
+    },
+    {
+      name: 'Pessoa 4',
+      age: 40
+    }
+  ] 
+
+  selectPerson(index: number) {
+    this.personSelectedIndex = index
+  }
 }
