fdc48b3e59281e11a55e6289a62fc9a896b3a9f6
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-14 09:17:48 -0300

    Pipes

--- 03_Binding_Diretivas_Templates_Decorators/pipes/src/app/app.component.ts
+++ 03_Binding_Diretivas_Templates_Decorators/pipes/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  text: string = 'Texto'
+  person = {
+    name: 'Nome da pessoa 1',
+    status: 1
+  }
+
+  person2 = {
+    name: 'Nome da pessoa 2',
+    status: 2
+  }
+
+  person3 = {
+    name: 'Nome da pessoa 3',
+    status: 3
+  }
+
+  getStyle(status: number) {
+    return {
+      'active': status === 1,
+      'partial': status === 2,
+      'blocked': status === 3,
+    }
+  }
   
 }
