ed8e49b9980136ec6e9003bf41b7ae7b3b98b46d
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-14 08:22:33 -0300

    NgStyle

--- 03_Binding_Diretivas_Templates_Decorators/ngstyle/src/app/app.component.ts
+++ 03_Binding_Diretivas_Templates_Decorators/ngstyle/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  fontSize: number = 15
+  textColor: 'green' | 'orange' = 'orange'
+  buttonText: 'Verde' | 'Laranja' = 'Verde'
+  stylesString: string = 'font-size: 15px; color: orange;'
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor
+  }
+
+  increaseFontSize() {
+    this.fontSize += 5
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor}`
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    }
+  }
+
+  toggleFontColor() {
+    if (this.textColor === 'green') {
+      this.textColor = 'orange'
+      this.buttonText = 'Verde'
+    } else {
+      this.textColor = 'green'
+      this.buttonText = 'Laranja'
+    }
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor}`
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    }
+  }
+
 }
