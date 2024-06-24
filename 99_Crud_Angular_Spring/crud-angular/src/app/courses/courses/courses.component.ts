110931f0fcdc3b7b5703c0addc45ab1bbfbce1ad
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-24 13:49:16 -0300

    Crud - Tratamento de erros e MatDialog

--- 99_Crud_Angular_Spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ 99_Crud_Angular_Spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -14,10 +16,25 @@ export class CoursesComponent implements OnInit {
 
   displayedColumns = ['name', 'category']
 
-  constructor(private coursesService: CoursesService) { 
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) { 
     this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos')
+          return of( [])
+        })
+      )
   }
 
   ngOnInit(): void { }
+
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage,
+    });
+  }
 
 }
