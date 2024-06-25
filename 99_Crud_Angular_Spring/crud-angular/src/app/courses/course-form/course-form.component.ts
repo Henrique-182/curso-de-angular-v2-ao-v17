47d7f8ef7e1c498c453d0acaedb795b896ee4196
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-25 10:55:22 -0300

    Crud - Tratamentos de Erros

--- 99_Crud_Angular_Spring/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ 99_Crud_Angular_Spring/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +12,11 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder, 
+    private service: CoursesService,
+    private _snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -19,8 +25,19 @@ export class CourseFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onSubmit() {}
+  onSubmit() {
+    this.service
+      .save(this.form.value)
+      .subscribe({
+        next: (data) => console.log(data),
+        error: () => this.onError()
+      })
+  }
 
   onCancel() {}
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso', '', { duration: 5000 })
+  }
 
 }
